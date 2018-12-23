39c4abd707d862075c60ed8502c3edb298a756a9
bhumimalla <bhumimalla7@gmail.com>
2018-12-23 23:00:07 +0545

    jsp changed to html

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 def dashboard(request):
-    #return HttpResponse("<h1>Hello</h1>")
+    # return HttpResponse("<h1>Hello</h1>")
     return render(request, "dashboard/dashboard.html")
